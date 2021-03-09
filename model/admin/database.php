eb5b4880ab5bfb42b459b4c000f01b89e2709b74
TaylorCookie <77705936+TaylorCookie@users.noreply.github.com>
2021-03-09 17:52:40 -0600

    Update database.php

--- model/admin/database.php
+++ model/admin/database.php
@@ -1,11 +1,11 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    // $password = 'pa55word';
+    $dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $username = 'wsmqzoye8zau43y6';
+    $password = 'vn5gbfr6bdt99lpn';
 
     try {
-        $db = new PDO($dsn, $username);
-        // $db = new PDO($dsn, $username, $password);
+        //$db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
 
     } catch (PDOException $e) {
         $error_message = 'Database Error: ';
