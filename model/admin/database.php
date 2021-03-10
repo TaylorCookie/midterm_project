2b880d9c0f5c3b5ae15fe1e18faaf8fac62d3e5e
Taylor Cook <tecook2@mail.fhsu.edu>
2021-03-09 20:29:19 -0600

    Update database.php

--- model/admin/database.php
+++ model/admin/database.php
@@ -1,11 +1,14 @@
 <?php
-    $dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $host = 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $dbname = 'zps2pkd5j62t6e6u';
+    //$dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     $username = 'wsmqzoye8zau43y6';
     $password = 'vn5gbfr6bdt99lpn';
 
     try {
         //$db = new PDO($dsn, $username);
-        $db = new PDO($dsn, $username, $password);
+        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
     } catch (PDOException $e) {
         $error_message = 'Database Error: ';
